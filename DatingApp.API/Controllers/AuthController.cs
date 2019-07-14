366e50d65a1ea05333b860ebb4bc5b87b77870dc
Fahir <fahir@live.com>
2019-07-14 03:50:48 +0200

    Set Error interceptor in Angular for error handlings

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -49,7 +49,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer says no!");
 
              var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
@@ -79,7 +78,7 @@ namespace DatingApp.API.Controllers
             return Ok(new {
                 token = tokenHandler.WriteToken(token)
             });
-            
+
         }
     }
 }
