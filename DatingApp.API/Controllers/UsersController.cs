35dfee9b5b1449636ad72fa7a3fe8b7e65b64442
Fahir <fahir@live.com>
2019-07-20 18:02:48 +0200

    Set up the paging helper class

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -28,6 +28,7 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
+            
             var users = await _repo.GetUsers();
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
