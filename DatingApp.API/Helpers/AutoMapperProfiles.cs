eec091daa91c144ee3d56e2d66c51b193c32aa9f
Fahir <fahir@live.com>
2019-07-21 15:36:24 +0200

    Added the Create Message method in the API

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -28,6 +28,7 @@ namespace DatingApp.API.Helpers
            CreateMap<Photo,PhotoForReturnDto>();
            CreateMap<PhotoForCreatingDto,Photo>();
            CreateMap<UserForRegisterDto, User>();
+           CreateMap<MessageForCreationDto, Message>().ReverseMap();
 
         }
     }
